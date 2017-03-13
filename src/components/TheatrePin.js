0e92ab47f4b86691bc10f409645559807c111b37
Arun-prasad7 <arunprasad2017@kellogg.northwestern.edu>
2017-03-12 23:45:10 -0500

    edit 1

---
+++ src/components/TheatrePin.js
@@ -0,0 +1,17 @@
+// React
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+// Google Maps
+var ReactGMaps = require('react-gmaps')
+var {Gmaps, Marker} = ReactGMaps
+
+var TheatrePin = React.createClass({
+  
+  render: function() {
+    return 
+        
+  }  
+})
+
+module.exports = TheatrePin;
